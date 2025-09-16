90d022d51b20c1d5f636906f85475c30482ca9c0
Praveenkumar dasari <dasaripraveenkumar2000@gmail.com>
2025-09-16 14:58:16 +0530

    #1 praveen sidebar

---
+++ src/components/IconsData.jsx
@@ -0,0 +1,16 @@
+import { Icon } from "@iconify/react";
+
+const IconsData = {
+    'hamburger': <Icon icon="garden:menu-stroke-16" width="16" height="16" style={{marginTop: '2px'}} />,
+    'overall-dashbaord': <Icon icon="hugeicons:menu-square" width="16" height="16" style={{marginTop: '2px'}} />,
+    'robot-face': <Icon icon="meteor-icons:robot" width="16" height="16" style={{marginTop: '2px'}} />,
+    'alert-bell': <Icon icon="fluent:alert-on-16-regular" width="16" height="16" style={{marginTop: '2px'}} />,
+    'logs' : <Icon icon="icon-park-outline:upload-logs" width="16" height="16" style={{marginTop: '2px'}} />,
+    'info' : <Icon icon="quill:info" width="16" height="16" style={{marginTop: '2px'}} />, 
+    'settings': <Icon icon="fluent:settings-28-regular" width="16" height="16" style={{marginTop: '2px'}} />,
+    'signal-strength': <Icon icon="uil:signal" width="16" height="16" style={{marginTop: '2px'}} />,
+    'battery': <Icon icon="mdi:battery-outline" width="16" height="16" style={{marginTop: '2px'}} />,
+    'location': <Icon icon="mdi:location" width="16" height="16" style={{marginTop: '2px'}} />,
+}
+
+export default IconsData;
---
+++ src/utils/SelectedTabStyle.js
@@ -0,0 +1,4 @@
+const SelectedTabStyle = (tabLink, currentPageLink) =>
+  tabLink === currentPageLink ? "selected-tab" : "unselected-tab";
+
+export default SelectedTabStyle;
